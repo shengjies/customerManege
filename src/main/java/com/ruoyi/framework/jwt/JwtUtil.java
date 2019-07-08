96987e2e68e34a8d0e7b7ab99cc2de107d513234
zqm <1305219970@qq.com>
2019-07-08 11:11:17 +0800

    获取在线用户

--- src/main/java/com/ruoyi/framework/jwt/JwtUtil.java
+++ src/main/java/com/ruoyi/framework/jwt/JwtUtil.java
@@ -1,6 +1,7 @@
 package com.ruoyi.framework.jwt;
 
 import com.alibaba.fastjson.JSON;
+import com.ruoyi.common.utils.ServletUtils;
 import com.ruoyi.common.utils.StringUtils;
 import com.ruoyi.project.system.user.domain.User;
 import io.jsonwebtoken.Claims;
@@ -14,90 +15,102 @@ import java.util.Map;
 
 public class JwtUtil {
 
-public static final String CLAIM_KEY_USER="sub";
-    public static final String CLAIM_KEY_SECRET="secret";
+    public static final String CLAIM_KEY_USER = "sub";
+    public static final String CLAIM_KEY_SECRET = "secret";
 
     /**
      * 进行信息加密
+     *
      * @param claims
      * @return
      */
-    public static String getToken(Map<String,Object> claims){
+    public static String getToken(Map<String, Object> claims) {
         return Jwts.builder()
                 .setClaims(claims)
                 .setExpiration(getExpirationDate())
-                .signWith(SignatureAlgorithm.HS256,CLAIM_KEY_SECRET)
+                .signWith(SignatureAlgorithm.HS256, CLAIM_KEY_SECRET)
                 .compact();
     }
 
     /**
      * 获取存活时间
+     *
      * @return
      */
-    public static Date getExpirationDate(){
-        return new Date(System.currentTimeMillis()+12*60*60*1000);
+    public static Date getExpirationDate() {
+        System.out.println();
+        return new Date(System.currentTimeMillis() + 60 * 1000);
     }
 
     /**
      * token 解析
+     *
      * @param token
      * @return
      */
-    public static Claims getClaimsFromToken(String token){
-        Claims claims =null;
+    public static Claims getClaimsFromToken(String token) {
+        Claims claims = null;
         try {
-             claims  = Jwts.parser()
+            claims = Jwts.parser()
                     .setSigningKey(CLAIM_KEY_SECRET)
                     .parseClaimsJws(token)
                     .getBody();
-        }catch (Exception e){
+        } catch (Exception e) {
             //e.printStackTrace();
         }
-        return  claims;
+        return claims;
     }
 
-    public static User getUserByToken(String token){
-        User userInfo =null;
+    public static User getUserByToken(String token) {
+        User userInfo = null;
         try {
             //判断解析结果是否为空
             Claims claims = getClaimsFromToken(token);
-            if(claims !=null){
+            if (claims != null) {
                 String subject = claims.getSubject();
-                userInfo = JSON.parseObject(subject,User.class);
+                userInfo = JSON.parseObject(subject, User.class);
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return  userInfo;
+        return userInfo;
     }
 
-    public static User getTokenUser(HttpServletRequest request){
-       return getTokenCookie(request.getCookies());
+    public static User getTokenUser(HttpServletRequest request) {
+        return getTokenCookie(request.getCookies());
     }
 
-    public static User getTokenCookie(Cookie[] cookies){
+    public static User getTokenCookie(Cookie[] cookies) {
         String token = null;
-        if(cookies == null)return null;
+        if (cookies == null) return null;
         for (Cookie cookie : cookies) {
-            if(cookie.getName().equals("token")){
+            if (cookie.getName().equals("token")) {
                 token = cookie.getValue();
             }
         }
-        if(StringUtils.isEmpty(token)){
+        if (StringUtils.isEmpty(token)) {
             return null;
         }
         return getUserByToken(token);
     }
 
-    public static String getToken(HttpServletRequest request){
+    public static String getToken(HttpServletRequest request) {
         String token = null;
         Cookie[] cookies = request.getCookies();
-        if(cookies == null)return token;
+        if (cookies == null) return token;
         for (Cookie cookie : cookies) {
-            if(cookie.getName().equals("token")){
+            if (cookie.getName().equals("token")) {
                 token = cookie.getValue();
             }
         }
         return token;
+    }
+
+    /**
+     * 获取用户
+     * @return 用户
+     */
+    public static User getUser(){
+         return getTokenUser(ServletUtils.getRequest());
     }
 }
